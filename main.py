42045d42e45b6cfc7ec5063632b17e8c44a38399
nbovv <oleky.oz@gmail.com>
2025-04-26 23:08:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv(TOKEN)
 
 intents = discord.Intents.default()
 intents.message_content = True
