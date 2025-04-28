9c1b99c8f804483bbc59fc7903a7b75e0fee84ea
nbovv <oleky.oz@gmail.com>
2025-04-28 16:30:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -432,14 +432,14 @@ async def warn(interaction: discord.Interaction, members: str, powod: str, month
         embed.add_field(name="Użytkownik", value=member.mention, inline=False)
         embed.add_field(name="Warn", value=f"{nowy_warn}/3", inline=True)
         embed.add_field(name="Powód", value=powod, inline=False)
-        if nowy_warn == 3:
-            try:
-        # Sprawdzamy czy użytkownik NIE ma obecnie aktywnego timeouta
-                if not member.timed_out_until or member.timed_out_until < datetime.utcnow():
-                    await member.edit(timed_out_until=datetime.utcnow() + timedelta(days=1), reason="Przekroczenie 3/3 WARN — przerwa na 1 dzień")
-                    embed.add_field(name="Akcja", value="🛑 Nadano timeout na **1 dzień** za przekroczenie 3/3 WARN.", inline=False)
-            except Exception as e:
-                print(f"❌ Błąd przy dawaniu timeouta: {e}")
+        if mial_3_warn:
+    try:
+        if not member.timed_out_until or member.timed_out_until < datetime.utcnow():
+            await member.edit(timed_out_until=datetime.utcnow() + timedelta(days=1), reason="Przekroczenie 3/3 WARN — przerwa na 1 dzień")
+            embed.add_field(name="Akcja", value="🛑 Nadano timeout na **1 dzień** za przekroczenie 3/3 WARN.", inline=False)
+    except Exception as e:
+        print(f"❌ Błąd przy dawaniu timeouta: {e}")
+
 
 
 
