ad839b4117fdd1502d19aeb738f3fe3b8e8e150b
nbovv <oleky.oz@gmail.com>
2025-05-31 21:00:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ logging.basicConfig(level=logging.INFO)
 from discord import ui
 import random
 from discord import Interaction
+from discord import Embed, Color
 
 DATA_FILE = "/var/data/dealer_data.json"
 
